f45f9ef7bfa2b3b121fbe5968559885ceb6989ad
555Tim555 <s10114105@gmail.com>
2020-03-13 21:25:59 +0800

    homework

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -6,12 +6,12 @@ public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter number of tickets:");
-        int ticket = scanner.nextInt();                             //輸入購買張數
+        int ticket = scanner.nextInt();                     //輸入購買張數
         System.out.println("How many round-trip tickets:");
-        int rt_ticket = scanner.nextInt();                          //輸入來回票的張數
+        int rt_ticket = scanner.nextInt();                  //輸入來回票的張數
 
-        train Ticket = new train(ticket,rt_ticket);   //建立train_ticket的類別物件，並設初始值
-        Ticket.total_monly();                                       //呼叫改買的總金額並印出
+        train Ticket = new train(ticket,rt_ticket);         //建立train_ticket的類別物件，並設初始值
+        Ticket.total_monly();                               //呼叫改買的總金額並印出
 
     }
 }
