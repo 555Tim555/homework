b12f04cf73e21d69c7361c3d4a1a910fbd932db3
555Tim555 <s10114105@gmail.com>
2020-03-20 21:06:07 +0800

    homework2

--- src/main/java/com/train/train.java
+++ src/main/java/com/train/train.java
@@ -4,7 +4,7 @@ public class Train {
     int total_ticket;   //購買的總張數
     int rTicket;        //來回票的張數
 
-    public Train(int ticket,int rt_ticket){         //設定購買的總張數和來匯票張數
+    public Train(int ticket,int rt_ticket){         //設定購買的總張數和來回票張數
         this.total_ticket = ticket;
         this.rTicket = rt_ticket;
     }
