92f47ce79ebf2e0f0b05ac00c55a2d1589641a97
555Tim555 <s10114105@gmail.com>
2020-03-20 20:55:13 +0800

    homework_2

--- src/main/kotlin/TesterKotlin.kt
+++ src/main/kotlin/TesterKotlin.kt
@@ -2,19 +2,30 @@ import java.util.*
 
 fun main() {
     val scanner =Scanner(System.`in`)
-    print("Please enter number of tickets:")
-    var ticket = scanner.nextInt()              //輸入購買張數
-    print("How many round-trip tickets:")
-    var rt_ticket = scanner.nextInt()           //輸入來回票的張數
 
-    val Ticket = Train_Ticket(ticket,rt_ticket) //建立Train_Ticket變數，並給於初始值
-    Ticket.total_monly()                        //呼叫改買的總金額並印出
+    var ticket = 0
+    var rTicket = 0
+    var Ticket = Train_Ticket(ticket,rTicket)            //建立Train_Ticket變數，並給於初始值
+    while(true){
+        print("Please enter number of tickets:")
+        ticket = readLine()!!.toInt()                       //輸入購買張數
+        if(ticket == -1)break                               //判斷是否結束訂票
+        print("How many round-trip tickets:")
+        rTicket = readLine()!!.toInt()                      //輸入來回票的張數
+        Ticket = Train_Ticket(ticket,rTicket)
+        Ticket.total_monly()                                //呼叫改買的總金額並印出
+    }
+    println("End")
+
+
+
 }
 
 class Train_Ticket(var ticket : Int,var rt_ticket : Int){
     fun total_monly(){
+        var monly = ((ticket-rt_ticket)*1000+(rt_ticket*2000*0.9)).toInt()
         println("Total tickets:$ticket")       //印出購買的總張數
         println("Round-trip:$rt_ticket")        //印出來回票張數
-        println("Total:" + ((ticket-rt_ticket)*1000+(rt_ticket*2000*0.9)).toInt())  //印出總金額
+        println("Total: $monly")  //印出總金額
     }
 }
