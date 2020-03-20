3b8afb3fa5958f610779dc407375de55d074bc73
555Tim555 <s10114105@gmail.com>
2020-03-20 21:01:23 +0800

    homework2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter number of tickets:");
-        int ticket = scanner.nextInt();                     //輸入購買張數
-        System.out.println("How many round-trip tickets:");
-        int rt_ticket = scanner.nextInt();                  //輸入來回票的張數
 
-        train Ticket = new train(ticket,rt_ticket);         //建立train_ticket的類別物件，並設初始值
-        Ticket.total_monly();                               //呼叫改買的總金額並印出
+        int ticket = 0;
+        int rTicket = 0;
+        Train Ticket = new Train(ticket,rTicket);
 
+        while(true)
+        {
+            System.out.println("Please enter number of tickets:");
+            ticket = scanner.nextInt();                            //輸入購買張數
+            if(ticket == -1)break;                                 //判斷是否結束訂票
+            System.out.println("How many round-trip tickets:");
+            rTicket = scanner.nextInt();                           //輸入來回票的張數
+            Ticket = new Train(ticket,rTicket);         //建立train_ticket的類別物件，並設初始值
+            Ticket.total_monly();                               //呼叫改買的總金額並印出
+
+        }
+        System.out.println("End");
     }
 }
--- src/main/java/com/train/train.java
+++ src/main/java/com/train/train.java
@@ -1,16 +1,16 @@
 package com.train;
 
-public class train {
-    int total_ticket;//購買的總張數
-    int rt_ticket;//來回票的張數
+public class Train {
+    int total_ticket;   //購買的總張數
+    int rTicket;        //來回票的張數
 
-    public train(int ticket,int rt_ticket){         //設定購買的總張數和來匯票張數
+    public Train(int ticket,int rt_ticket){         //設定購買的總張數和來匯票張數
         this.total_ticket = ticket;
-        this.rt_ticket = rt_ticket;
+        this.rTicket = rt_ticket;
     }
     public void total_monly(){
         System.out.println("Total tickets:" + total_ticket);        //印出購買的總張數
-        System.out.println("Round-trip:" + rt_ticket);              //印出來回票張數
-        System.out.println("Total:" + (int)((total_ticket-rt_ticket)*1000+(rt_ticket*2000*0.9)));   //印出總金額
+        System.out.println("Round-trip:" + rTicket);                //印出來回票張數
+        System.out.println("Total:" + (int)((total_ticket-rTicket)*1000+(rTicket*2000*0.9)));   //印出總金額
     }
 }
